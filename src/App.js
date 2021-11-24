3fb7d910d0b79009d762b3053dee9b6b73cd5c9d
arijitghosh10 <arijighosh2331@gmail.com>
2021-11-25 00:30:57 +0530

    updated appjs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import './App.css'
 import Calculator from "./components/Calculator";
+import Convertor from "./components/Convertor";
 
 function App() {
+  const [showConvertor, setShowConvertor] = useState(false);
+  const [toggle, setToggle] = useState(false)
+  const switchHAndler = () => {
+    setShowConvertor(!showConvertor);
+    setToggle(!toggle)
+  }
   return (
-    <div className="wrapper">    
-      <Calculator />
+    <div className="wrapper">
+      <button className="switchBtn" onClick={switchHAndler}>Switch to {toggle ? "Calculator" : "Convertor"}</button>
+      {!showConvertor && <Calculator />}
+      {showConvertor && <Convertor />}
     </div>
   );
 }
